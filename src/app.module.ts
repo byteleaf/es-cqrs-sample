34476e981f0e13238b7d963c5d6e8923dff0e42f
Stefan Sauterleute <stefan.sauterleute@byteleaf.de>
2025-03-21 17:19:00 +0100

    remove app (controller + service)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { EventStoreModule } from './event-store/event-store.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [EventStoreModule],
 })
 export class AppModule {}
