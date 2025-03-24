c262cd4a4af8487cc0f26c9e2887a90c23072c04
Stefan Sauterleute <stefan.sauterleute@byteleaf.de>
2025-03-24 16:02:42 +0100

    update sql migration

---
+++ prisma/migrations/20250324144627_initial_book_schema/migration.sql
@@ -0,0 +1,27 @@
+-- CreateEnum
+CREATE TYPE "BookStatus" AS ENUM ('AVAILABLE', 'BORROWED', 'DAMAGED', 'REPAIRED', 'REMOVED');
+
+-- CreateTable
+CREATE TABLE "events" (
+    "id" TEXT NOT NULL,
+    "aggregate_id" TEXT NOT NULL,
+    "aggregate_revision" INTEGER NOT NULL,
+    "type" TEXT NOT NULL,
+    "time_observed" TIMESTAMP(3) NOT NULL,
+    "time_occurred" TIMESTAMP(3) NOT NULL,
+    "data" JSONB NOT NULL,
+
+    CONSTRAINT "events_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "books" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "isbn" TEXT NOT NULL,
+    "status" "BookStatus" NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "books_pkey" PRIMARY KEY ("id")
+);
