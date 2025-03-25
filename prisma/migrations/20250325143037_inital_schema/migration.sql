8475fa3db82e1557a53e9d0746dfe81275cfd3f1
Stefan Sauterleute <stefan.sauterleute@byteleaf.de>
2025-03-25 15:34:06 +0100

    clean up

--- prisma/migrations/20250325143037_inital_schema/migration.sql
+++ prisma/migrations/20250325143037_inital_schema/migration.sql
@@ -25,3 +25,20 @@ CREATE TABLE "books" (
 
     CONSTRAINT "books_pkey" PRIMARY KEY ("book_id")
 );
+
+-- CreateTable
+CREATE TABLE "snapshots" (
+    "id" TEXT NOT NULL,
+    "aggregate_id" TEXT NOT NULL,
+    "revision" INTEGER NOT NULL,
+    "state" JSONB NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "snapshots_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "events_aggregate_id_idx" ON "events"("aggregate_id");
+
+-- CreateIndex
+CREATE INDEX "snapshots_aggregate_id_idx" ON "snapshots"("aggregate_id");
